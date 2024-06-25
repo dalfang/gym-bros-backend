f3f48d0eb53bd393d768e8d653fa25eb3865f11f
[Dalin Fangloy] <[d.fangloy@gmail.com]>
2024-06-25 15:30:50 +0200

    update progress model

--- models/Progress.model.js
+++ models/Progress.model.js
@@ -1,12 +1,13 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
 
 const progressSchema = new Schema({
-    date: { type: Date, required: true },
-    routines: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Routine' }],
-    meals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meal' }],
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-  }, { timestamps: true });
-  
+  date: { type: Date, required: true },
+  routines: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Routine' }],
+  meals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meal' }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
 const Progress = model("Progress", progressSchema);
 
 module.exports = Progress;
