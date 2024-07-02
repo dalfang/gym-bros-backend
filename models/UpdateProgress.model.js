48f1556edafda37b71e1f41ae08341c553ac361d
Farnoush Daliran <farnoushfdf@gmail.com>
2024-07-02 18:31:59 +0200

    make update progress model

---
+++ models/UpdateProgress.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+// const progressSchema = new Schema({
+//   date: { type: Date, required: true },
+//   routines: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Routine' }],
+//   meals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meal' }],
+//   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+// }, { timestamps: true });
+
+const updateProgressSchema = new Schema({
+  water: {
+    type: Number,
+    required: true,
+    // default: 0,
+  },
+  weight: {
+    type: Number,
+    required: true,
+    // default: 0,
+  },
+  workout: {
+    type: Number,
+    required: true,
+    // default: 0,
+  },
+  sleep: {
+    type: Number,
+    required: true,
+    // default: 0,
+  },
+  walk: {
+    type: Number,
+    required: true,
+    // default: 0,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "UserModel",
+    // required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Progress = model("Progress", progressSchema);
+
+module.exports = Progress;
