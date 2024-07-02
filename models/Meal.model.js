2a70233e3adb5fcc7e5f37f38ae23845b67e7ad4
Farnoush Daliran <farnoushfdf@gmail.com>
2024-07-02 16:04:39 +0200

    change meal model

--- models/Meal.model.js
+++ models/Meal.model.js
@@ -1,17 +1,21 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const mealSchema = new Schema({
-    name: { 
-        type: String, 
-        enum: ["Breakfast", "Lunch", "Dinner", "Snack", "Other"],
-        required: true, 
+const mealSchema = new Schema(
+  {
+    name: {
+      type: String,
+      enum: ["Breakfast", "Lunch", "Dinner", "Snack", "Other"],
+      required: true,
     },
     description: { type: String },
     calories: { type: Number },
-    owner: { type: Schema.ObjectId, ref: 'UserModel' }, 
+    owner: { type: Schema.ObjectId, ref: "UserModel" },
     ingredients: [{ type: String }],
     date: { type: Date, required: true },
-}, { timestamps: true });
+    image: { type: String },
+  },
+  { timestamps: true }
+);
 
 const Meal = model("Meal", mealSchema);
 
